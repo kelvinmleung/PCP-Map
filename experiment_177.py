c83b25c2f39b22634efb7323d8716debaaed2fdb
Xialu <zhengxialu@gmail.com>
2024-06-06 21:58:47 +0800

    update

--- experiment_177.py
+++ experiment_177.py
@@ -3,8 +3,8 @@ import numpy as np
 import pandas as pd
 
 # TODO change to correct paths
-loss = pd.read_csv('.../PCP-Map/experiments/tabcond/lv_valid_hist.csv').to_numpy()
-param = pd.read_csv('.../PCP-Map/experiments/tabcond/lv_params_hist.csv').to_numpy()
+loss = pd.read_csv('.../PCP-Map/experiments/tabcond/177_valid_hist.csv').to_numpy()
+param = pd.read_csv('.../PCP-Map/experiments/tabcond/177_params_hist.csv').to_numpy()
 loss_param = np.concatenate((param[:, 1:], loss[:, 1:]), axis=1)
 unique_param = loss_param[np.unique(loss_param[:, :-1], return_index=True, axis=0)[1]]
 unique_param = unique_param[unique_param[:, -1].argsort()]
@@ -15,6 +15,8 @@ lr = param_list[1]
 width = int(param_list[2])
 width_y = int(param_list[3])
 num_layers = int(param_list[4])
+
+
 
 os.system(
     "python train_cond.py --data 'lv' --valid_freq 50 --early_stopping 20  --input_x_dim 4 --input_y_dim 9\
